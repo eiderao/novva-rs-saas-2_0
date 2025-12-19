13866bb57d06a738abc135fcfea19f19ad2646a3
Eider Arantes de Oliveira <eider.arantes.oliveira@gmail.com>
2025-12-19 16:13:09 -0300

    Adicionando lucide-react ao package.json

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../supabase/client';
 import { 
   ArrowLeft, 
   User, 
